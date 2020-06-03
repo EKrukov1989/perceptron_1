2b2c4a096f47ab56209ff9d7807d6b325d5ebbdb
Evgeny Kryukov <evgenykryukov@mail.ru>
2020-06-03 20:52:09 +0300

    Added class for GraphWindow

---
+++ src/graphwindow.py
@@ -0,0 +1,76 @@
+"""Module for GraphWindow-class."""
+
+import tkinter as tk
+
+
+class GraphWindow():
+    """The class incapsulated all logic for graph window.
+
+    It creates GUI-elements, draw and redraw data
+    """
+
+    __graph_canvas = None
+    __points = None
+    __line = None
+
+    def __redraw(self):
+        """Redraw all in graph window."""
+        w = self.__graph_canvas.winfo_width()
+        h = self.__graph_canvas.winfo_height()
+
+        # sx & sy - means screen coordinates
+        def sx(x): return 0.5 * w * (1 + x)
+        def sy(y): return 0.5 * h * (1 - y)
+
+        self.__graph_canvas.delete('axis')
+        self.__graph_canvas.delete('points')
+        self.__graph_canvas.delete('line')
+
+        # draw axis
+        self.__graph_canvas.create_line(sx(-1), sy(0), sx(1), sy(0),
+                                        tags='axis', fill='blue')
+        self.__graph_canvas.create_line(sx(0), sy(-1), sx(0), sy(1),
+                                        tags='axis', fill='red')
+        # draw points
+        if self.__points is not None:
+            for pt in self.__points:
+                x, y = sx(pt[0]), sy(pt[1])
+                sh = 4  # shift
+                self.__graph_canvas.create_oval((x-sh, y-sh, x+sh, y+sh),
+                                                tags='points', fill='green',
+                                                outline='green')
+
+        # draw line
+        if self.__line is not None:
+            for i in range(1, len(self.__line)):
+                x0, x1 = sx(self.__line[i-1][0]), sx(self.__line[i][0])
+                y0, y1 = sy(self.__line[i-1][1]), sy(self.__line[i][1])
+                self.__graph_canvas.create_line(x0, y0, x1, y1, width=2,
+                                                tags='line', fill='black')
+
+    def __init__(self, parent_frame):
+        """Create GUI-elements and attach them to parent."""
+        self.__graph_canvas = tk.Canvas(parent_frame)
+        self.__graph_canvas['bg'] = 'white'
+        self.__graph_canvas['highlightthickness'] = 0
+        self.__graph_canvas.place(anchor='nw', relwidth=1.0, relheight=1.0)
+
+        self.__graph_canvas.bind('<Configure>', lambda e: self.__redraw())
+
+    def set_points(self, points_array):
+        """Set 2d-array of points.
+
+        Each point will be drawn as circle in graph window.
+        All coordinates must be in the nterval [-1;1]
+        """
+        self.__points = points_array
+        self.__redraw()
+
+    def set_line(self, line_array):
+        """Set 2d-array of points for line.
+
+        This points will be used for drawing line in graph window.
+        All coordinates must be in the nterval [-1;1]
+        """
+        self.__line = line_array
+        self.__redraw()
--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@
 This module consists all logic of top level
 """
 
-import tkinter as ttk
+import tkinter as tk
 import tkinter.font as ttkfont
+from graphwindow import GraphWindow
 
 FRAME_HALF_PAD = 10
 BACKGROUND_COLOR = '#666666'
@@ -12,63 +13,52 @@ BACKGROUND_COLOR = '#666666'
 
 def attach_graph_frame(root):
     """Add canvas for representation of graphic."""
-    graph_frame = ttk.Frame(root)
+    graph_frame = tk.Frame(root)
     graph_frame['bg'] = BACKGROUND_COLOR
     graph_frame.place(anchor='nw', relwidth=0.5, relheight=0.5)
-    graph_frame_inner = ttk.Frame(graph_frame)
+    graph_frame_inner = tk.Frame(graph_frame)
     graph_frame_inner.pack(expand=True, fill='both',
                            padx=(2*FRAME_HALF_PAD, FRAME_HALF_PAD),
                            pady=(2*FRAME_HALF_PAD, FRAME_HALF_PAD))
-    graph_canvas = ttk.Canvas(graph_frame_inner)
-    graph_canvas['bg'] = 'white'
-    graph_canvas['highlightthickness'] = 0
-    graph_canvas.place(anchor='nw', relwidth=1.0, relheight=1.0)
-
-    def redraw(e):
-        graph_canvas.delete('axis')
-        graph_canvas.create_line(0, 0.5*e.height, e.width, 0.5*e.height,
-                                 tags='axis', fill='blue')
-        graph_canvas.create_line(0.5*e.width, 0, 0.5*e.width, e.height,
-                                 tags='axis', fill='red')
-    graph_canvas.bind('<Configure>', redraw)
+    return GraphWindow(graph_frame_inner)
 
 
 def attach_controls_frame(root):
     """Add controls."""
-    controls_frame = ttk.Frame(root)
+    controls_frame = tk.Frame(root)
     controls_frame['bg'] = BACKGROUND_COLOR
     controls_frame.place(anchor='nw', rely=0.5, relwidth=0.5, relheight=0.5)
-    controls_frame_inner = ttk.Frame(controls_frame)
+    controls_frame_inner = tk.Frame(controls_frame)
     controls_frame_inner['bg'] = BACKGROUND_COLOR
     controls_frame_inner.pack(expand=True, fill='both',
                               padx=(2*FRAME_HALF_PAD, FRAME_HALF_PAD),
                               pady=(FRAME_HALF_PAD, 2*FRAME_HALF_PAD))
 
-    launchButton = ttk.Button(controls_frame_inner, text="Launch", width=12)
-    clearButton = ttk.Button(controls_frame_inner, text="Clear", width=12)
+    launchButton = tk.Button(controls_frame_inner, text="Launch", width=12)
+    clearButton = tk.Button(controls_frame_inner, text="Clear", width=12)
     launchButton.grid(row=0, column=0, padx=(0, 10))
     clearButton.grid(row=0, column=1)
 
 
 def attach_logs_frame(root):
     """Add text area for logs."""
-    logs_frame = ttk.Frame(root)
+    logs_frame = tk.Frame(root)
     logs_frame['bg'] = BACKGROUND_COLOR
     logs_frame.place(anchor='nw', relx=0.5, relwidth=0.5, relheight=1.0)
-    logs_frame_inner = ttk.Frame(logs_frame)
+    logs_frame_inner = tk.Frame(logs_frame)
     logs_frame_inner.pack(expand=True, fill='both',
                           padx=(FRAME_HALF_PAD, 2*FRAME_HALF_PAD),
                           pady=(2*FRAME_HALF_PAD, 2*FRAME_HALF_PAD))
 
-    logs_area = ttk.Text(logs_frame_inner, wrap=ttk.NONE)
+    logs_area = tk.Text(logs_frame_inner, wrap=tk.NONE)
     logs_area.insert('1.0', 'Area for logs')
     logs_area['font'] = ttkfont.Font(family='Consolas', size=12)
-    logs_area.config(state=ttk.DISABLED)
+    logs_area.config(state=tk.DISABLED)
     logs_area.grid(row=0, column=0, sticky='NSEW')
 
-    yscroll = ttk.Scrollbar(logs_frame_inner)
+    yscroll = tk.Scrollbar(logs_frame_inner)
     yscroll.grid(row=0, column=1, sticky='NS')
-    xscroll = ttk.Scrollbar(logs_frame_inner, orient=ttk.HORIZONTAL)
+    xscroll = tk.Scrollbar(logs_frame_inner, orient=tk.HORIZONTAL)
     xscroll.grid(row=1, column=0, sticky='WE')
 
     logs_area.config(yscrollcommand=yscroll.set)
@@ -84,14 +74,19 @@ def attach_logs_frame(root):
 
 def main():
     """Start application."""
-    root = ttk.Tk()
+    root = tk.Tk()
 
     root.wm_iconbitmap('src/images/window.ico')
     root.minsize(800, 600)
     root.state('zoomed')
     root.wm_title('perceptron_1')
 
-    attach_graph_frame(root)
+    graph_window = attach_graph_frame(root)
+    points = [[-0.5, -0.5], [0.5, 0.5]]
+    line = [[-0.4, -0.4], [0.4, 0.4]]
+    graph_window.set_points(points)
+    graph_window.set_line(line)
+
     attach_logs_frame(root)
     attach_controls_frame(root)
 
